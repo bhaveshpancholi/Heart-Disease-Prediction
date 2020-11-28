9a31e794e2010dd2b9673cea282708efe5ec889c
BhaveshPancholi08 <bhavesh.pancholi@tigeranalytics.com>
2020-11-28 12:01:41 +0530

    added input prompt

--- testing.py
+++ testing.py
@@ -1,1 +1,9 @@
 print("Hey!, This file just for testing and learning purpose.")
+
+x=input('do you want to learn git?')
+
+if x=='yes':
+    print('All the best!')
+
+elif x=='no':
+    print('May I know why you do not want to learn.')
